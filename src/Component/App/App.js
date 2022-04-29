56f3aad8b0b52ab0e6cb56d79755fc99238e95cb
ahmed <904645@mof.gov.sa>
2022-04-30 01:05:35 +0200

    from 26 to 33

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,44 +1,91 @@
-import { useState } from "react";
-import Card from "../Card/Card";
+import { useState, useRef } from "react";
 import styles from "./App.module.css";
+import Card from "../Card/Card";
+import Filter from "../Filter/Filter";
 
 const App = () => {
-  const [cardsToggle, setCardToggle] = useState(false);
+  //const inputEL = useRef(null);
 
+  const [cardsToggle, setCardToggle] = useState(true);
+  const [filter, setFilter] = useState("");
   const [state, setState] = useState([
     {
+      id: 1,
       name: "Ahmed",
       age: 30,
       email: "Ahmed@gmail.com",
       city: "Cairo",
       phone: "132113",
+      type: "boy",
     },
     {
+      id: 2,
       name: "Hassan",
       age: 40,
       email: "Hassan@gmail.com",
       city: "6 October",
       phone: "2423455",
+      type: "boy",
     },
     {
+      id: 3,
       name: "Ali",
       age: 50,
       email: "Ali@gmail.com",
       city: "El-Shrook",
       phone: "1156453",
+      type: "boy",
+    },
+    {
+      id: 4,
+      name: "Salwa",
+      age: 50,
+      email: "Salwa@gmail.com",
+      city: "El-Shrook",
+      phone: "1156453",
+      type: "girl",
+    },
+    {
+      id: 5,
+      name: "Alaa",
+      age: 50,
+      email: "Alaa@gmail.com",
+      city: "El-Shrook",
+      phone: "1156453",
+      type: "girl",
+    },
+    {
+      id: 6,
+      name: "Maha",
+      age: 50,
+      email: "Maha@gmail.com",
+      city: "El-Shrook",
+      phone: "1156453",
+      type: "girl",
     },
   ]);
 
-  const deleteHandler = (e, index) => {
+  const deleteHandler = (e, id) => {
     //const deleteOperation = state.filter((item, i) => i !== index);
     //setState(deleteOperation);
-    setState((prevState) => prevState.filter((item, i) => i !== index));
+    setState((prevState) => prevState.filter((item, i) => item.id !== id));
     console.log("-------------------------------------");
     console.log(state);
   };
 
   const toggleHandler = () => {
     setCardToggle(!cardsToggle);
+  };
+  const filterNames = (name) => {
+    setFilter(name);
+  };
+
+  const namesHandler = () => {
+    if (filter.length === 0) return state;
+
+    return state.filter((item) =>
+      item.name.toLowerCase().includes(filter.toLowerCase())
+    );
   };
 
   return (
@@ -47,8 +94,11 @@ const App = () => {
       <button style={{ marginBottom: "20px" }} onClick={toggleHandler}>
         {cardsToggle ? "Hide Names" : "Show Names"}
       </button>
+
       <div className={cardsToggle ? styles.show : styles.hide}>
-        <Card arrayCard={state} type="Men" deleteFunc={deleteHandler} />
+        <Filter filteration={filterNames}></Filter>
+
+        <Card arrayCard={namesHandler()} deleteFunc={deleteHandler} />
       </div>
     </div>
   );
--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,26 +1,28 @@
 import React from "react";
 import styles from "././Card.module.css";
 
-const Card = ({ arrayCard, type, deleteFunc }) => {
-  const cards = arrayCard.map(({ name, age, email, city, phone }, index) => (
-    <div
-      className={styles.cardWrapper}
-      key={index}
-      style={{ backgroundColor: type === "Men" ? "Silver" : "pink" }}
-    >
-      <div>{name}</div>
-      <div>{age}</div>
-      <div>{email}</div>
-      <div>{city}</div>
-      <div>{phone}</div>
+const Card = ({ arrayCard, deleteFunc }) => {
+  const cards = arrayCard.map(
+    ({ name, age, email, city, phone, type, id }, index) => (
       <div
-        className={styles.deleteBtn}
-        onClick={(event) => deleteFunc(event, index)}
+        className={styles.cardWrapper}
+        key={id}
+        style={{ backgroundColor: type === "boy" ? "Silver" : "pink" }}
       >
-        X
+        <div>{name}</div>
+        <div>{age}</div>
+        <div>{email}</div>
+        <div>{city}</div>
+        <div>{phone}</div>
+        <div
+          className={styles.deleteBtn}
+          onClick={(event) => deleteFunc(event, id)}
+        >
+          X
+        </div>
       </div>
-    </div>
-  ));
+    )
+  );
 
   return <div>{cards}</div>;
 };
